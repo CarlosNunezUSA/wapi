d44ff52d50b28293e76181c8f418da69c9009e39
Carlos I. Nunez <carlos@compexc.com>
2016-05-13 17:18:16 -0400

    Added serialization and compression. Need to test.

---
+++ HermesFramework/Utils/Compressor.vb
@@ -0,0 +1,61 @@
+﻿ 
+'========================================================================================
+' Copyright (c) 2016 Carlos I. Nunez. All rights reserved.
+'========================================================================================
+'
+'	Project File:	HermesFramework / Utils.Compressor.vb
+'	Created on:		5/13/2016 @ 5:10 PM
+'	Modified on:	5/13/2016 @ 5:12 PM 
+'	Author:			Nunez, Carlos 
+' 
+'========================================================================================
+ 
+Imports System.IO
+Imports System.IO.Compression
+Imports System.Text
+
+Namespace Utils
+    Friend NotInheritable Class Compressor
+
+        Private Sub New()
+        End Sub
+
+        Public Shared Function Zip(text As String) As String
+            Dim buffer__1 As Byte() = Encoding.UTF8.GetBytes(text)
+            Dim memoryStream = New MemoryStream()
+            Using gZipStream = New GZipStream(memoryStream, CompressionMode.Compress, True)
+                gZipStream.Write(buffer__1, 0, buffer__1.Length)
+            End Using
+
+            memoryStream.Position = 0
+
+            Dim compressedData = New Byte(memoryStream.Length - 1) {}
+            memoryStream.Read(compressedData, 0, compressedData.Length)
+
+            Dim gZipBuffer = New Byte(compressedData.Length + 3) {}
+            Buffer.BlockCopy(compressedData, 0, gZipBuffer, 4, compressedData.Length)
+            Buffer.BlockCopy(BitConverter.GetBytes(buffer__1.Length), 0, gZipBuffer, 0, 4)
+            Return Convert.ToBase64String(gZipBuffer)
+        End Function
+
+
+        Public Shared Function UnZip(compressedText As String) As String
+            Dim gZipBuffer As Byte() = Convert.FromBase64String(compressedText)
+            Using memoryStream = New MemoryStream()
+                Dim dataLength As Integer = BitConverter.ToInt32(gZipBuffer, 0)
+                memoryStream.Write(gZipBuffer, 4, gZipBuffer.Length - 4)
+
+                Dim buffer = New Byte(dataLength - 1) {}
+
+                memoryStream.Position = 0
+                Using gZipStream = New GZipStream(memoryStream, CompressionMode.Decompress)
+                    gZipStream.Read(buffer, 0, buffer.Length)
+                End Using
+
+                Return Encoding.UTF8.GetString(buffer)
+            End Using
+        End Function
+
+    End Class
+
+End Namespace
---
+++ HermesFramework/Utils/Serializer.vb
@@ -0,0 +1,71 @@
+﻿
+'========================================================================================
+' Copyright (c) 2016 Carlos I. Nunez. All rights reserved.
+'========================================================================================
+'
+'	Project File:	HermesFramework / Utils.Serializer.vb
+'	Created on:		5/13/2016 @ 4:51 PM
+'	Modified on:	5/13/2016 @ 4:52 PM 
+'	Author:			Nunez, Carlos 
+' 
+'========================================================================================
+
+Imports System.IO
+Imports System.Runtime.Serialization.Formatters.Binary
+
+Namespace Utils
+    Public Class FileSerializer
+
+        Public Property FileName As String
+
+        Public Sub SerializeToFile(of T)(o As T)
+            Try
+                If File.Exists(filename) Then
+                    File.Delete(filename)
+                End If
+                ' serialize
+                Dim serialized As Byte() = BinarySerializer.Serialize(o)
+                ' to string
+                Dim o64 As String = compressor.Zip(Convert.ToBase64String(serialized))
+                ' to file
+                File.AppendAllText(filename, o64)
+            Catch
+                Throw
+            End Try
+        End Sub
+
+        Public Function RestoreSerializedObject(of T) As T
+            Dim result As New List(Of String)
+            ' from file
+            Dim ostring As String = File.ReadAllText(filename)
+            Dim serialized As Byte() = Convert.FromBase64String(compressor.UnZip(ostring))
+            Return BinarySerializer.Deserialize(Of T)(serialized)
+        End Function
+
+    End Class
+
+
+    Public Class BinarySerializer
+
+        Shared Function Serialize(ByVal data As Object) As Byte()
+            Try
+                If TypeOf data Is Byte() Then Return data
+                Using M As New IO.MemoryStream : Dim F As New BinaryFormatter : F.Serialize(M, data) : Return M.ToArray() : End Using
+            Catch ex As Exception
+                Throw
+            End Try
+        End Function
+
+        Shared Function Deserialize(Of T)(ByVal data As Byte()) As T
+            Try
+                Using M As New IO.MemoryStream(data, False) : Return CType((New BinaryFormatter).Deserialize(M), T) : End Using
+            Catch ex As Exception
+                Throw
+            End Try
+        End Function
+
+    End Class
+
+
+
+End Namespace
